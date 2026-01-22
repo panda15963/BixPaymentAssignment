0b5b29d3c651cefce9d9f253f7af77b19a13f31c
MinseokChoi <seok7521@gmail.com>
2026-01-22 22:19:25 +0900

    패키지 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from "@vercel/analytics/react"
 import './index.css'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import RootLayout from "./layouts/RootLayout.tsx";
 import Main from "./components/Main.tsx";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-      <BrowserRouter>
-          <Routes>
-              <Route element={<RootLayout />}>
-                  <Route index element={<Main/>}/>
-              </Route>
-          </Routes>
-      </BrowserRouter>
-  </StrictMode>,
+    <StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route element={<RootLayout />}>
+                    <Route index element={<Main/>}/>
+                </Route>
+            </Routes>
+            <Analytics />
+        </BrowserRouter>
+    </StrictMode>,
 )
